d5bda0e061fc55b3a0af6c8a5f83e7262987d124
William Wang <wilwang@microsoft.com>
2026-05-08 02:23:07 +0000

    Merged PR 2089944: Admin Schema Role Change
    ### Does this PR have any customer impact?
    Yes
    ### Type (Feature, Refactoring, Bugfix, DevOps, Testing, Perf, etc)
    Schema
    ### Does it involve schema level changes? (Table, Column, Index, UDF, etc level changes)
    Yes
    ### Are you introducing any new config? If yes, do you have tests with and without them being set?
    No
    ### ChangeLog (Refer [Template](../oss/CHANGELOG.md))
    1.113
    ### Description
    Changing Admin role grants so that Gateway test infrastructure can use non-SUPERUSER, and instead use Admin role
    
    Signed-off-by: William Wang <wilwang@microsoft.com>

--- pg_documentdb/sql/schema/roles_metadata--0.113-0.sql
+++ pg_documentdb/sql/schema/roles_metadata--0.113-0.sql
@@ -2,15 +2,15 @@
 -- so it can manage custom roles via createRole/dropRole.
 DO $$
 BEGIN
-    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'documentdb_root_role') THEN
-        EXECUTE 'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE documentdb_api_catalog.roles TO documentdb_root_role';
-        EXECUTE 'GRANT USAGE ON SCHEMA documentdb_api_catalog, documentdb_core, documentdb_data, documentdb_api, documentdb_api_internal TO documentdb_root_role WITH GRANT OPTION';
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'documentdb_admin_role') THEN
+        EXECUTE 'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE documentdb_api_catalog.roles TO documentdb_admin_role';
+        EXECUTE 'GRANT USAGE ON SCHEMA documentdb_api_catalog, documentdb_core, documentdb_data, documentdb_api, documentdb_api_internal TO documentdb_admin_role WITH GRANT OPTION';
     END IF;
 END $$;
 
 DO $$
 BEGIN
-    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'documentdb_root_role') THEN
-        EXECUTE 'GRANT USAGE ON SCHEMA documentdb_api_internal_readonly TO documentdb_root_role WITH GRANT OPTION';
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'documentdb_admin_role') THEN
+        EXECUTE 'GRANT USAGE ON SCHEMA documentdb_api_internal_readonly TO documentdb_admin_role WITH GRANT OPTION';
     END IF;
 END $$;
