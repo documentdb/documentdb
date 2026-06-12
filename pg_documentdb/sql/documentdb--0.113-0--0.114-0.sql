dd0006670802a8226eb4e87104b657a539ed3381
William Wang <wilwang@microsoft.com>
2026-06-12 02:30:25 +0000

    Merged PR 2148726: Roles Table Primary Key Revert
    ### Does this PR have any customer impact?
    Yes
    ### Type (Feature, Refactoring, Bugfix, DevOps, Testing, Perf, etc)
    Revert
    ### Does it involve schema level changes? (Table, Column, Index, UDF, etc level changes)
    Yes
    ### Are you introducing any new config? If yes, do you have tests with and without them being set?
    No
    ### ChangeLog (Refer [Template](../oss/CHANGELOG.md))
    1.114
    ### Description
    We are reverting the PR for changing the primary key of roles table
    
    Signed-off-by: Guanzhou Song <guanzhousong@microsoft.com>
    Signed-off-by: William Wang <wilwang@microsoft.com>

--- pg_documentdb/sql/documentdb--0.113-0--0.114-0.sql
+++ pg_documentdb/sql/documentdb--0.113-0--0.114-0.sql
@@ -3,6 +3,3 @@
 #include "udfs/query/bson_query_match--0.114-0.sql"
 #include "udfs/commands_crud/cursor_functions--0.114-0.sql"
 #include "udfs/schema_validation/schema_validation--0.114-0.sql"
-
--- Roles table: migrate primary key from role_oid to role_name
-#include "schema/roles_metadata--0.114-0.sql"
